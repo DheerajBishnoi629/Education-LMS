094e31491980870f6ee59ee01a371b716766666a
Dheeraj-Bishnoi <dheerub02@gmail.com>
2026-06-29 21:08:37 +0530

    project-setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Main } from './shared/components/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Main, title: 'Home' },
+];
